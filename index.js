e2e22d462d6dfd7847064823924eafd5d682d52c
Igor Veyner <igorveyner95@gmail.com>
2020-02-27 11:10:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ function theBeatlesPlay(musicians, instruments){
   return plays;
 }
 
-function johnLennonFacts(){
-  const facts = [
+const facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
     "He hated the sound of his own voice"
   ];
+  
+function johnLennonFacts(facts){
   
 }
